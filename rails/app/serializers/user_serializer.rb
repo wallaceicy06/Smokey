b3213690c37638e750d363b606a43e08a1d51757
Sean Harger <seanharger93@gmail.com>
2015-07-20 10:09:13 -0700

    Updated user serializer to include email.

--- rails/app/serializers/user_serializer.rb
+++ rails/app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name
+  attributes :id, :username, :first_name, :last_name, :email
   has_many :invited_jacks, serializer: JackForUserSerializer, include: false, embed: :id
 end
