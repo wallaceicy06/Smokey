57389def42f6108ee346ddc15331cef7e29c5d27
Sean Harger <seanharger93@gmail.com>
2015-07-21 11:06:57 -0700

    Validates uniquess of a jack invitation to one user and one jack.

--- rails/app/models/jack_invitation.rb
+++ rails/app/models/jack_invitation.rb
@@ -2,5 +2,5 @@ class JackInvitation < ActiveRecord::Base
   belongs_to :user
   belongs_to :jack
   validates :user, presence: true
-  validates :jack, presence: true
+  validates :jack, presence: true, uniqueness: {scope: :user}
 end
